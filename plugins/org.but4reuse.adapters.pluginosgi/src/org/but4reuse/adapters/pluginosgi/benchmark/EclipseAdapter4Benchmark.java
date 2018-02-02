84d73825bb06fb7c7825980d3b973b45b047a68f
aarkoub <Amel^^@Amel-pc>
2018-02-02 17:12:10 +0100

    Ajout de l'option pour Plugin-OSGI

---
+++ plugins/org.but4reuse.adapters.pluginosgi/src/org/but4reuse/adapters/pluginosgi/benchmark/EclipseAdapter4Benchmark.java
@@ -0,0 +1,30 @@
+package org.but4reuse.adapters.pluginosgi.benchmark;
+
+import java.util.List;
+
+import org.but4reuse.adapters.IElement;
+import org.but4reuse.adapters.pluginosgi.EclipseAdapter;
+import org.but4reuse.adapters.pluginosgi.FileElement;
+import org.but4reuse.adapters.pluginosgi.PluginElement;
+
+/**
+ * Eclipse adapter for benchmark. Do not include file elements
+ */
+public class EclipseAdapter4Benchmark extends EclipseAdapter {
+
+	@Override
+	protected void addElement(List<IElement> elements, FileElement newElement) {
+		if (newElement instanceof PluginElement) {
+			elements.add(newElement);
+
+			// System.out.println(((PluginElement)newElement).getSymbName());
+
+			// if(((PluginElement)newElement).getName() == null ||
+			// ((PluginElement)newElement).getName().contains("%")){
+			// System.out.println(((PluginElement)newElement).getSymbName() +
+			// " " + ((PluginElement)newElement).getName());
+			// }
+		}
+	}
+
+}
