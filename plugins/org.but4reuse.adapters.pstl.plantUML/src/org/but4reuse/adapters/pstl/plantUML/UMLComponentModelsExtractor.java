88653e02eed920d0d138375e208ad23a730d5742
3301571 <3301571@ppti-14-401-13.ufr-info-p6.jussieu.fr>
2018-04-24 12:59:37 +0200

    uml

--- plugins/org.but4reuse.adapters.pstl.plantUML/src/org/but4reuse/adapters/pstl/plantUML/UMLComponentModelsExtractor.java
+++ plugins/org.but4reuse.adapters.pstl.plantUML/src/org/but4reuse/adapters/pstl/plantUML/UMLComponentModelsExtractor.java
@@ -1,5 +1,6 @@
 package org.but4reuse.adapters.pstl.plantUML;
 
+import java.io.File;
 import java.util.List;
 
 import org.but4reuse.adaptedmodel.AdaptedModel;
@@ -22,27 +23,18 @@ public class UMLComponentModelsExtractor {
 	public static void createComponentModels(String constructionURI, AdaptedModel adaptedModel) {
 
 			System.out.println("Start construct plant UML component models");
-			
-			//////////*************************************/////
-			/// To be adapted for UML component Model ///
-			
-			//////////*************************************/////
-			
-			
-			/**
-			 * Construct the Base Model
-			 */
-			// TODO For the construction of the BaseModel refactor to use
-			// org.but4reuse.adapters.emf.helper.EMFHelper.constructMaximalEMFModel(AdaptedModel,
-			// List<IElement>, URI)
-			// Get emf adapter
-			// retrieve the common block
+
 			List<IElement> elements ;
-			List<Block> blocks = AdaptedModelHelper.getCommonBlocks(adaptedModel);
-						
+			List<Block> blocks = adaptedModel.getOwnedBlocks();
+			
+			 File directory = new File(constructionURI);
+			 if (! directory.exists()){
+			    directory.mkdirs();
+			 }
+			
 			for(int i=0; i<blocks.size(); i++){
 				elements = AdaptedModelHelper.getElementsOfBlock(blocks.get(i));
-				UML.generateUMLDiagram(elements, "./Block"+i+"Uml.txt", "./Block"+i+"Uml.png");
+				UML.generateUMLDiagram(elements, constructionURI+"Block"+i+"Uml.txt", constructionURI+"Block"+i+"Uml.png");
 			}
 			
 			System.out.println("END construct plant UML component models");
